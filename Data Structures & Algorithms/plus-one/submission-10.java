2bb4007879dfeea7f17f71c53f6d9aad89e851d9
Harish-Coded <harish2510140@ssn.edu.in>
2026-07-16 01:39:58 +0530

    Add: plus-one - submission-10

---
+++ Data Structures & Algorithms/plus-one/submission-10.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] plusOne(int[] n) {
+        int i = n.length-1;
+        while(i>=0)
+        {
+            if(n[i]!=9) 
+            {
+                n[i]=n[i]+1;
+                return n;
+            }
+            else
+            {
+                n[i]=0;
+                i--;
+            }
+        }
+        int ls = n.length;
+        if(i==-1)
+        {
+            ls++;
+        }
+        int[] num = new int[ls];
+        num[0]=1;
+        for(int j=1;j<ls;j++)
+        {
+            num[j]=0;
+        }
+        return num;
+    }
+}
