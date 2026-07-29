83d36d1b2c21705bb4ad7093de32236f33c7c95a
Harish-Coded <harish2510140@ssn.edu.in>
2026-07-30 00:16:35 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = Arrays.binarySearch(nums, target);
+        if(index>=0) return index;
+        return -1;
+    }
+}
